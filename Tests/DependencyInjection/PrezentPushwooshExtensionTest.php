992fd7c2ccdb3d6d0e955054f66f2a47cc3cda56
Robert-Jan Bijl <robert-jan@prezent.nl>
2016-04-29 08:22:31 +0200

    update configuration test with new logging parameter

--- Tests/DependencyInjection/PrezentPushwooshExtensionTest.php
+++ Tests/DependencyInjection/PrezentPushwooshExtensionTest.php
@@ -41,6 +41,7 @@ class PrezentPushwooshExtensionTest extends \PHPUnit_Framework_TestCase
         $applicationGroupId = 'YYY-YYYY';
         $apiKey = 'xxxxxxxxxxxxxx';
         $clientClass = 'Gomoob\Pushwoosh\Client\PushwooshMock';
+        $logRequests = true;
 
         $this->extension->load(
             [
@@ -49,6 +50,7 @@ class PrezentPushwooshExtensionTest extends \PHPUnit_Framework_TestCase
                     'application_group_id' => $applicationGroupId,
                     'api_key' => $apiKey,
                     'client_class' => 'Gomoob\Pushwoosh\Client\PushwooshMock',
+                    'log_requests' => true,
                 ]
             ],
             $this->container
@@ -57,6 +59,7 @@ class PrezentPushwooshExtensionTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($applicationId, $this->container->getParameter('prezent_pushwoosh.application_id'));
         $this->assertEquals($applicationGroupId, $this->container->getParameter('prezent_pushwoosh.application_group_id'));
         $this->assertEquals($apiKey, $this->container->getParameter('prezent_pushwoosh.api_key'));
+        $this->assertEquals($logRequests, $this->container->getParameter('prezent_pushwoosh.log_requests'));
 
         $client = $this->container->get('pushwoosh');
         $this->assertEquals($clientClass, get_class($client));
